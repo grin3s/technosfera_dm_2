0ebfd2cc0c93941cc553a3d3828578832d4b260f
Alexander Gringauz <grin3s@mail.ru>
2015-11-13 18:51:34 +0300

    fetched work place coordinated

--- hw2.py
+++ hw2.py
@@ -2,6 +2,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import requests
+import json
+import pickle
+
+GOOGLE_API_KEY = "AIzaSyDd8Ybx4xStlfY927OC6DcCzhYnkTWlwxw"
+GOOGLE_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json?address="{0}"&key={1}'
+
+LOAD_FROM_FILE = True
 
 
 def plot_bar_counts(data):
@@ -18,10 +26,33 @@ def violin_plot_cat(data, cat, val, order):
     sns.boxplot(x=val, y=cat, data=data, order=order)
     sns.stripplot(x=val, y=cat, data=data, jitter=True, size=4, order=order);
 
+
+def fetch_location(location):
+    full_url = GOOGLE_API_URL.format(location, GOOGLE_API_KEY);
+    answer = requests.get(full_url).json()
+    results = answer['results']
+    if len(results) > 0:
+        position = results[0]['geometry']['location']
+        return position['lat'], position['lng']
+    else:
+        return None
+
+
+def get_all_locations(data):
+    res = dict()
+    all_locations = data.groupby('Work Location').groups.keys()
+    for idx, location in enumerate(all_locations):
+        print "{0} out of {1}".format(idx, len(all_locations))
+        pos = fetch_location(location)
+        res[location] = pos
+
+    return res
+
 dataset = pd.read_csv('NYC_Jobs.csv')
 grouped_agency = dataset.groupby('Agency')
 grouped_agency_count = grouped_agency.size().sort_values(ascending=False)
 dataset['AvgSalary'] = (dataset['Salary Range From'] + dataset['Salary Range To']) / 2
-plot_bar_counts(grouped_agency_count)
+locations = get_all_locations(dataset)
+#plot_bar_counts(grouped_agency_count)
 
-violin_plot_cat(dataset[dataset['Salary Frequency']=="Annual"],'Agency','AvgSalary',grouped_agency_count.keys().values)
+#violin_plot_cat(dataset[dataset['Salary Frequency']=="Annual"],'Agency','AvgSalary',grouped_agency_count.keys().values)
